faa217f6f045fd1f228ec23be362367d865aa716
Natalia Herrera Neira <la.nataliaherrera@MacBook-Pro-de-Natalia.local>
2024-04-03 15:21:38 -0300

    feat: implement jwt

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 require("./config/database").connect()
 const express = require("express")
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken")
 
 const User = require('./model/user')
 
-const app = express()
+const app = express() 
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -12,15 +14,44 @@ app.get("/", (req, res) => {
 })
 
 app.post("/register", async (req, res) => {
-    const { firstname, lastname, email, password}  = req.body
+    try {
+        const { firstname, lastname, email, password}  = req.body
 
-    if (!(email && password && lastname && firstname)) {
-        res.status(400).send('All fields are required')
+        if (!(email && password && lastname && firstname)) {
+            res.status(400).send('All fields are required')
+        }
+    
+        const existingUser = await User.findOne({email})
+        
+        if (existingUser) res.status(401).send("User already exists")
+    
+        const myEncPassword = await bcrypt.hash(password, 10)
+    
+        const user = await User.create({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            password: myEncPassword
+        })
+    
+        //token
+        const token = jwt.sign(
+            {user_id: user._id, email},
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "2h"
+            }
+        )
+        user.token = token
+        //update or not
+
+        //handle passwords situation
+        user.password = undefined   
+        res.status(201).json(user) 
+    } catch (error) {
+        console.log(error);
     }
 
-    const existingUser = await User.findOne({email}) // PROMISE
-    
-    if (existingUser) res.status(401).send("User already exists")
 })
 
 
